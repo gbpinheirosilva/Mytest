8be5fcd0066e172af805b38cd3e7074d29b3555d
gbpinheirosilva <gbpinheirosilva@gmail.com>
2019-11-22 10:11:02 -0300

    This is the second brach created.

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ import numpy as np
 a = np.array([1,2,3])
 b = np.array([1,2,3])
 #This is just a comment to test if the branch is working
+#This is the comment of the new branch mytest-branch-0.2
 c = a+b
 print(c)
